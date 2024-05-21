6de2e54756da665df3123fb3b2bb475fd5e1fa94
Kevin Taylor <ktay296@wgu.edu>
2024-05-21 11:40:15 -0400

    Updated Calculator

--- calculator.py
+++ calculator.py
@@ -8,24 +8,45 @@ print('[1] Addition\n[2] Subtraction\n[3] Division\n[4] Multiplication\n[5] Powe
 
 #Takes the input from the user from the list provided
 math = int(input('Enter number for the type of Math problem please: '))
+equation = ''
 #Checks if they select 0 and tells them goodbye and exits program
 if math == 0:
     print('Goodbye!')
     sys.exit()
+elif math == 1:
+    print('You selected ADDITION')
+elif math == 2:
+    print('You selected SUBTRACTION')
+elif math == 3:
+    print('You selected DIVISION')
+elif math == 4:
+    print('You selected MULTIPLICATION')
+elif math == 5:
+    print('You selected TO THE POWER OF X')
+elif math > 5:
+    print('Sorry, that is not a selection.')
+    sys.exit()
 firstNumber = int(input('Enter first number: '))
+print('You entered: ', firstNumber)
 secondNumber = int(input('Enter second number (if you selected 5 this will be the exponent): '))
+print('You entered: ', secondNumber)
 
 if math == 1:
     answer = firstNumber + secondNumber #Adding both numbers
+    equation = 'plus'
 elif math == 2:
     answer = firstNumber - secondNumber #Subtracting both numbers
+    equation = 'minus'
 elif math == 3:
     answer = firstNumber / secondNumber #Dividing both Numbers
+    equation = 'divided by'
 elif math == 4:
     answer = firstNumber * secondNumber #Multiplying both Numbers
+    equation = 'times'
 elif math == 5:
     answer = firstNumber ** secondNumber #Rasing to the power of X
+    equation = 'to the power of'
     
 
-print(answer)
+print(firstNumber, equation, secondNumber, 'equals', answer)
                     
