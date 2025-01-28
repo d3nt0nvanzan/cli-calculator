83301646eb2b36968df2eb3ed2a92330bc7ff195
d3nt0nvanzan <142911042+d3nt0nvanzan@users.noreply.github.com>
2025-01-28 12:16:05 -0500

    Add files via upload

--- equate.py
+++ equate.py
@@ -1,9 +1,9 @@
 def equate(math):
     math_operations = {
-        1: 'plus',
-        2: 'minus',
-        3: 'divided by',
-        4: 'times',
-        5: 'to the power of'
+        '+': 'plus',
+        '-': 'minus',
+        '/': 'divided by',
+        '*': 'times',
+        '^': 'to the power of'
     }
     return math_operations.get(math, 'Invalid operation')
